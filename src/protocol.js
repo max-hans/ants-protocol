8654fb6c3d2da056163fae8914f321aac8cae9b5
max-hans <m.hans@me.com>
2020-11-15 10:38:50 +0100

    initial

---
+++ src/protocol.js
@@ -0,0 +1,9 @@
+module.exports = {
+  CONNECTION_STATUS: "connectionStatus",
+  DISCONNECTING: "disconnecting",
+  CONNECTING: "connecting",
+  HOME: "home",
+  GOTO: "goto",
+  STOP: "stop",
+  CONFIGURATION: "configuration",
+};
